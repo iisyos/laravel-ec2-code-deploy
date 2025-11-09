1be85822e2489b0673a4213d98d530c1eb7d8922
iisyos <kosuke1216a@yahoo.co.jp>
2025-11-09 22:58:37 +0900

    *feat: rename

--- terraform/code-deploy.tf
+++ terraform/code-deploy.tf
@@ -1,5 +1,5 @@
-module "code_build" {
-  source = "./modules/code-build"
+module "code_deploy" {
+  source = "./modules/code-deploy"
 
   app_name    = local.app_name
   instance_id = module.ec2.instance_id
